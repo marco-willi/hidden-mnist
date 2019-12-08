ad5ef3e2d0898e8318af4ed8c2cb641041023317
Marco Willi <will5448@umn.edu>
2019-12-08 17:59:34 +0100

    add examples

---
+++ README.md
@@ -0,0 +1,19 @@
+# Implementation of HiDDeN Network - Hiding Data in MNIST
+
+Based on _HiDDeN: Hiding data with deep networks (Zhu et al., 2018)_
+
+This is a preliminary implementation without any Noise layers.
+
+## Requirements
+
+- Tensorflow 2.0
+
+## Examples
+
+Here some examples after training on a few epochs. From left to right: Cover Image, Encoded Image, Difference Image.
+
+![image](examples.png)
+
+
+
+
--- main.py
+++ main.py
@@ -8,38 +8,26 @@ from matplotlib import pyplot as plt
 
 # Get Dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-#(x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
 
+# Transform Dataset
 data = x_train
 data = np.expand_dims(data, -1)
 input_shape = data.shape[1:]
 data = data.astype(np.float32)
-np.min(data)
+
 # Restrict to values between 0 and 1
-data = (data / 255)
-
-# Parameters
-msg_length = 8
-
-# msg = np.array([x for x in range(0, 32)])
-# msg = np.expand_dims(msg, 0)
-# xx = tf.keras.layers.RepeatVector(4*4)(msg)
-# xx.shape
-# xx[0,1,:]
-# yy = tf.keras.layers.Reshape((4, 4, 32))(xx)
-# yy.shape
-# yy[0, 0, 1,:]
+data = (data / 255.0)
 
 ####################
 # EncoderDecoder
 
 def encoder_net(cover_image, message, input_shape, msg_length):
-
+    """ Create Encoder Net """
+    
     # Message Block
-    mb = tf.keras.layers.RepeatVector(
+    m = tf.keras.layers.RepeatVector(
         input_shape[0] * input_shape[1])(message)
-    mb = tf.keras.layers.Reshape((input_shape[0:2]) + (msg_length, ))(mb)
-
+    m = tf.keras.layers.Reshape((input_shape[0:2]) + (msg_length, ))(m)
 
     # Image Processing Block
     x = cover_image
@@ -53,10 +41,10 @@ def encoder_net(cover_image, message, input_shape, msg_length):
         x = tf.keras.layers.BatchNormalization()(x)
         x = tf.keras.layers.LeakyReLU()(x)
 
-    # Concatenate Message Block with Image Processing Block and original image
-    x = tf.keras.layers.Concatenate(axis=-1)([mb, x, cover_image])
+    # Concatenate Message Block with Image Processing Block and Cover Image
+    x = tf.keras.layers.Concatenate(axis=-1)([m, x, cover_image])
 
-    # Encoded image
+    # Encode Image
     x = tf.keras.layers.Conv2D(
         filters=64,
         kernel_size=(3, 3),
@@ -75,6 +63,7 @@ def encoder_net(cover_image, message, input_shape, msg_length):
 
 
 def decoder_net(encoded_image, msg_length):
+    """ Decoder Net """
 
     x = encoded_image
 
@@ -96,31 +85,33 @@ def decoder_net(encoded_image, msg_length):
     x = tf.keras.layers.LeakyReLU()(x)
 
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(msg_length)(x)
-    return x
+    decoded_message = tf.keras.layers.Dense(msg_length)(x)
+    return decoded_message
 
 
 def encoder_decoder_net(input_shape, msg_length):
-    # inputs
+    """ EncoderDecoder Net """
     message = tf.keras.Input(shape=(msg_length, ), name='message')
     cover_image = tf.keras.Input(shape=input_shape, name='cover_image')
 
     encoded_image = encoder_net(cover_image, message, input_shape, msg_length)
     decoded_message = decoder_net(encoded_image, msg_length)
 
-    encoder_decoder = tf.keras.Model(
-        {'cover_image': cover_image, 'message': message},
+    model = tf.keras.Model(
+        inputs={
+            'cover_image': cover_image,
+            'message': message},
         outputs={
             'encoded_image': encoded_image,
             'decoded_message': decoded_message})
-    return encoder_decoder
-
+    return model
 
 
 ####################
-# Adversary
+# Discriminator
 
-def adversary_net(input_shape):
+def discriminator_net(input_shape):
+    """ Discriminator Net: Identify Encoded Images """
 
     image = tf.keras.Input(shape=input_shape)
     x = image
@@ -137,86 +128,95 @@ def adversary_net(input_shape):
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     logits = tf.keras.layers.Dense(1)(x)
 
-    adversary = tf.keras.Model(inputs=image, outputs=logits)
+    model = tf.keras.Model(inputs=image, outputs=logits)
 
-    return adversary
+    return model
 
 
 #################
 # input data
-def create_dataset(data, batch_size):
+
+def create_dataset(data, batch_size, shuffle_buffer=60000):
+    """ Dataset Iterator """
     dataset = tf.data.Dataset.from_tensor_slices(data)
     dataset = dataset.map(lambda x: tf.cast(x, dtype=tf.float32))
-    dataset = dataset.shuffle(60000)
+    dataset = dataset.shuffle(shuffle_buffer)
     dataset = dataset.batch(batch_size, drop_remainder=True)
     dataset = dataset.repeat(1)
     return dataset
 
-def reconstruction_loss(y, y_hat):
+def distortion_loss(y, y_hat):
+    """ Image Distortion Loss - L2 Norm """
     return tf.reduce_mean(tf.math.pow(y - y_hat, 2))
 
-def adversary_loss_cover(logits_cover):
-    """ Adversary Loss on Cover Images """
-    return tf.keras.losses.binary_crossentropy(
-        y_true=tf.zeros_like(logits_cover),
-        y_pred=logits_cover, from_logits=True)
+def recovery_loss(y, y_hat):
+    """ Message Recovery Loss - L2 Norm """
+    per_message = tf.reduce_mean(tf.math.pow(y - y_hat, 2), axis=0)
+    return tf.reduce_mean(per_message)
 
-def adversary_loss_encoded(logits_encoded)
-    """ Adversary Loss on Encoded Images """:
+def discriminator_loss(logits, y_expected):
+    """ Discriminator Loss """
+    y_true = tf.fill(logits.shape, y_expected)
     return tf.keras.losses.binary_crossentropy(
-        y_true=tf.ones_like(logits_encoded),
-        y_pred=logits_encoded, from_logits=True)
+        y_true=y_true,
+        y_pred=logits,
+        from_logits=True)
 
 def decoding_error_rate(y, y_hat):
+    """ Mean Error Rate across Batch """
     return tf.reduce_mean(tf.math.abs(y - tf.math.round(y_hat)), axis=0)
 
-
-#################
-# Create Architectures + optimizers
-encoder_decoder = encoder_decoder_net(input_shape, msg_length)
-adversary = adversary_net(input_shape)
-
-optimizer_encoder_decoder = tf.keras.optimizers.Adam()
-optimizer_adversary = tf.keras.optimizers.Adam()
 
 #################
 # Training Step
 
 @tf.function
-def train_step(cover_images, messages, optimizer_encoder_decoder, optimizer_adversary):
+def train_step(
+    cover_images,
+    messages,
+    optimizer_encoder_decoder,
+    optimizer_discriminator,
+    loss_weight_recover=1.0,
+    loss_weight_distortion=1.0,
+    loss_weight_adversarial=1.0):
     with tf.GradientTape() as tape_ed, tf.GradientTape() as tape_adv:
 
         encoder_decoder_output = encoder_decoder(
             inputs={'cover_image': cover_images, 'message': messages},
             training=True)
 
-        adverse_cover = adversary(
+        discriminator_on_cover = discriminator(
             inputs={'image': cover_images},
             training=True)
 
-        adverse_encoded = adversary(
+        discriminator_on_encoded = discriminator(
             inputs={'image': encoder_decoder_output['encoded_image']},
             training=True)
 
         # Loss of Encoder Decoder
-        loss_encoder = reconstruction_loss(
+        loss_distortion = distortion_loss(
             cover_images, encoder_decoder_output['encoded_image'])
 
-        loss_decoder = reconstruction_loss(
+        loss_recover = recovery_loss(
             messages, encoder_decoder_output['decoded_message'])
         
-        loss_detectability = tf.reduce_mean(adversary_loss_cover(adverse_encoded))
+        loss_adversarial = tf.reduce_mean(
+            discriminator_loss(discriminator_on_encoded, 0.0))
 
-        # loss of adversary
-        loss_adversary_cover = adversary_loss_cover(adverse_cover)
-        loss_adversary_encoded = adversary_loss_encoded(adverse_encoded)
+        # loss of Discriminator
+        loss_discriminator_cover = discriminator_loss(
+            discriminator_on_cover, 0.0)
+        loss_discriminator_encoded = discriminator_loss(
+            discriminator_on_encoded, 1.0)
 
-        # total loss adversary
-        loss_adversary = tf.reduce_mean(
-            loss_adversary_cover + loss_adversary_encoded)
+        # total loss discriminator
+        loss_discriminator = tf.reduce_mean(
+            loss_discriminator_cover + loss_discriminator_encoded)
 
         # total loss encoder decoder
-        loss_encoder_decoder = loss_encoder + loss_decoder + loss_detectability
+        loss_encoder_decoder = loss_weight_recover * loss_recover + \
+            loss_weight_distortion * loss_distortion + \
+            loss_weight_adversarial * loss_adversarial
 
     # gradient updates
     grads_encoder_decoder = tape_ed.gradient(
@@ -224,17 +224,22 @@ def train_step(cover_images, messages, optimizer_encoder_decoder, optimizer_adve
     optimizer_encoder_decoder.apply_gradients(
         zip(grads_encoder_decoder, encoder_decoder.trainable_variables))
 
-    grads_adversary = tape_adv.gradient(
-        loss_adversary, adversary.trainable_variables)
-    optimizer_adversary.apply_gradients(
-        zip(grads_adversary, adversary.trainable_variables))
+    grads_discriminator = tape_adv.gradient(
+        loss_discriminator, discriminator.trainable_variables)
+    optimizer_discriminator.apply_gradients(
+        zip(grads_discriminator, discriminator.trainable_variables))
     
-    # calculate message recovery error rate
-    mean_error_per_sample = decoding_error_rate(
-        messages, encoder_decoder_output['decoded_message'])
-    mean_error = tf.reduce_mean(mean_error_per_sample)
-        
-    return loss_encoder_decoder, loss_adversary
+    # Record losses
+    losses = {
+        'encoder_decoder': loss_encoder_decoder,
+        'discriminator': loss_discriminator,
+        'recover': loss_recover,
+        'distortion': loss_distortion,
+        'adversarial': loss_adversarial,
+        'discriminator_cover': tf.reduce_mean(loss_discriminator_cover),
+        'discriminator_encoded': tf.reduce_mean(loss_discriminator_encoded)}
+            
+    return losses
 
 
 #################
@@ -243,6 +248,18 @@ def train_step(cover_images, messages, optimizer_encoder_decoder, optimizer_adve
 batch_size = 32
 n_steps = 100
 n_epochs = 20
+msg_length = 8
+
+loss_weight_recover=1.0,
+loss_weight_distortion=0.7,
+loss_weight_adversarial=1e-3
+
+# Create Nets
+encoder_decoder = encoder_decoder_net(input_shape, msg_length)
+discriminator = discriminator_net(input_shape)
+
+optimizer_encoder_decoder = tf.keras.optimizers.Adam(1e-3)
+optimizer_discriminator = tf.keras.optimizers.Adam(1e-3)
 
 data_train = create_dataset(data, batch_size)
 
@@ -255,9 +272,12 @@ for e in range(0, n_epochs):
             [batch_size, msg_length], minval=0, maxval=2, dtype=tf.dtypes.int32)
         messages = tf.cast(messages, dtype=tf.dtypes.float32)
 
-        loss_de, loss_adv = train_step(
+        losses = train_step(
             cover_images, messages,
-            optimizer_encoder_decoder, optimizer_adversary)
+            optimizer_encoder_decoder, optimizer_discriminator,
+            loss_weight_recover=loss_weight_recover,
+            loss_weight_distortion=loss_weight_distortion,
+            loss_weight_adversarial=loss_weight_adversarial)
 
     # Plot Examples
     encoder_decoder_output = encoder_decoder(
@@ -269,9 +289,11 @@ for e in range(0, n_epochs):
     mean_error = tf.reduce_mean(mean_error_per_sample)
 
     print("==========================================")
-    print("Epoch: {} Loss EncoderDecoder: {:.5f}".format(e, loss_de))
-    print("Epoch: {} Loss Adversary: {:.5f}".format(e, loss_adv))
-    print("Epoch: {} Decoder Error Rate (1 batch): {:.2f}".format(e, mean_error))
+    for loss, loss_value in losses.items():
+        print("Epoch: {:<2} Loss {:<20}: {:.5f}".format(
+            e, loss, float(loss_value.numpy())))
+    print("Epoch: {:<2} Decoder Error Rate (1 batch): {:.2f}".format(
+        e, mean_error))
 
     for j in range(0, 2):
         img_cover = cover_images[j:j+1, :, :, :]
@@ -294,20 +316,33 @@ for e in range(0, n_epochs):
         plt.show()
 
 
+#################
+# Plot some figures
 
-messages
-tf.round(encoder_decoder_output['decoded_message'])
-
-adverse_encoded = adversary(
-    inputs={'image': encoder_decoder_output['encoded_image']},
+encoder_decoder_output = encoder_decoder(
+    inputs={'cover_image': cover_images, 'message': messages},
     training=False)
 
-tf.sigmoid(adverse_encoded)
-
-
-adverse_cover = adversary(
-    inputs={'image': cover_images},
-    training=False)
-
-adverse_cover
-tf.sigmoid(adverse_cover)
+n_rows = 6
+n_cols = 3
+index = 0
+fig = plt.figure(figsize=(4, 8))
+for j in range(0, n_rows):
+    img_cover = cover_images[j:j+1, :, :, :]
+    img_encoded = encoder_decoder_output['encoded_image'][j]
+    img_encoded = tf.squeeze(img_encoded, -1)
+    img_cover = np.squeeze(img_cover)
+    img_diff = np.abs(img_cover - img_encoded)  
+    plt.subplot(n_rows, n_cols, index + 1)
+    plt.imshow(img_encoded, cmap='gray')
+    plt.axis('off')
+    plt.subplot(n_rows, n_cols, index + 2)
+    plt.imshow(img_cover, cmap='gray')
+    plt.axis('off')
+    plt.subplot(n_rows, n_cols, index + 3)
+    plt.imshow(img_diff, cmap='gray')
+    plt.axis('off')
+    index += 3
+plt.tight_layout()
+plt.savefig('./examples.png', bbox_inches='tight',
+            pad_inches=0)
